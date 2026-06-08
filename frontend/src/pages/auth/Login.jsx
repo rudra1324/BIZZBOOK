edb4636ee913de770c395968d46fd040e6ef43a7
rudra1324 <rudraprataps1024@gmail.com>
2026-06-08 11:45:00 +0530

    Update Login.jsx

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -58,7 +58,7 @@ function Login() {
 
         const response =
           await fetch(
-            "http://localhost:5000/api/auth/login",
+            "https://bizzbook-c34x.onrender.com/api/auth/login",
             {
               method: "POST",
 
